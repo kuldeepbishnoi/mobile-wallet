4c06c799e95b592d14df7310383758a2022baef9
Kuldeep <kuldeepbishnoi.2001@gmail.com>
2023-10-26 10:20:10 +0530

    Delete microservice_version02_kafka_api_gateway_redis directory

--- microservice_version02_kafka_api_gateway_redis/transaction/transaction/src/main/java/com/mobilewallet/transaction/service/transactionService/TransactionService.java
+++
@@ -1,7 +0,0 @@
-package com.mobilewallet.transaction.service.transactionService;
-
-import com.mobilewallet.transaction.entity.Transaction;
-
-public interface TransactionService {
-    public void completeTransaction(Transaction transaction);
-}
